8d2fed7c28ef63f5be25b22b5d86c67ca78d1834
Ethan Smith <EthanKSmith@gmail.com>
2023-02-02 16:52:21 -0700

    Final

--- client/src/components/Grid.jsx
+++ client/src/components/Grid.jsx
@@ -5,20 +5,13 @@ import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry';
 
 
 function Grid() {
-
-  window.onload = () => {
-    const grid =document.querySelector('.grid');
-
-    const masonry = Masonry(grid);
-  };
-
   const  {birds}  = useOutletContext();
   return (
     <ResponsiveMasonry columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}>  
       <Masonry>    
         {birds &&  birds.map(bird => {
           return(
-          <div className='grid-item' key={bird._id}><Link to={`/birds/${bird._id}`}><img src={bird.image} alt={bird.name} style={{width:'100%', display: 'block'}} /></Link>
+          <div className='grid-item p-2' key={bird._id}><Link to={`/birds/${bird._id}`}><img src={bird.image} alt={bird.name} style={{width:'100%', display: 'block'}} /></Link>
           </div>
           )}
         )} 
